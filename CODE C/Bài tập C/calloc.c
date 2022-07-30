035c26cd36a8266de62c8e50eb2b6fbb0043c284
giangnam797vt@gmail.com <giangnam797vt@gmail.com>
2022-07-30 11:41:26 +0700

    update 2

--- CODE C/Bài tập C/calloc.c
+++ CODE C/Bài tập C/calloc.c
@@ -9,38 +9,46 @@
 int main()
 {
     // Create variables
-    int* pt;
+    int* pt,N;
     pt = (int* ) calloc(5, sizeof(int));
 
+    /* Khi dùng calloc, giá trị khởi tạo ban đầu cho con trỏ là bằng 0 */
+    printf("Gia tri cua con tro khi chua gan gia tri");
     for (int i = 0; i < 5; i++)
-    {
-        /* code */
-        printf("\ngia tri p[%d] = %d", i, *(pt+i));
-
-    }
+        printf("\np[%d] = %d", i, *(pt+i));
     printf("\n");
-        /* Khi dùng calloc, giá trị khởi tạo ban đầu cho con trỏ là bằng 0 */
 
+    printf("Gia tri cua con tro khi gan gia tri");
     for (int i = 0; i < 5; i++)
     {
-        /* code */
         *(pt + i ) = i;
-        printf("\ngia tri p[%d] = %d", i, *(pt+i));
+        printf("\np[%d] = %d", i, *(pt+i));
     }
     printf("\n");
+    printf("In gia tri cua vung nho");
     for (int i = 0; i < 5; i++)
+        printf("\ngia tri p[%d] = %d", i, *(pt+i));
+    printf("\n");
+    printf("\nCap phat them bo nho.\nSo phan tu them vao la N = ");
+    scanf("%d", &N);
+    pt = (int* )realloc(pt , N*sizeof(int));
+
+    for (int i = 5; i < 5+N; i++)
     {
-        /* code */
+        *(pt + i ) = i;
+        printf("\np[%d] = %d", i, *(pt+i));
+    }
+    printf("\n\nIn gia tri cua vung nho sau khi cap phat them.");
+
+    for (int i = 0; i < 5 + N; i++)
+    {
         printf("\ngia tri p[%d] = %d", i, *(pt+i));
     }
-    
     free(pt);
-     printf("\n");
-    for (int i = 0; i < 5; i++)
-    {
-        /* code */
-        printf("\ngia tri p[%d] = %d", i, *(pt+i));
-    }
+    // for (int i = 0; i < 5 + N; i++)
+    // {
+    //     printf("\ngia tri p[%d] = %d", i, *(pt+i));
+    // }
     return 0;
 }
 
--- CODE C/Bài tập C/malloc.c
+++ CODE C/Bài tập C/malloc.c
@@ -9,7 +9,7 @@
 int main()
 {
     // Create variables
-    int* pt, sum;
+    int* pt, N;
     pt = (int*) malloc(5*sizeof(int));
 
     // Nếu không thể cấp phát, 
@@ -20,39 +20,47 @@ int main()
         exit(0);
     }
     /* Khi dùng malloc, giá trị khởi tạo ban đầu cho con trỏ là các giá trị rác*/
+    printf("Gia tri cua con tro khi chua gan gia tri");
     for (int j = 0; j <5 ; j++)
     {
         /* code */
-        printf("\ngia tri thu p[%d] = %d ", j, *(pt+j));
+        printf("\np[%d] = %d ", j, *(pt+j));
     }
     printf("\n");
 
+    printf("Gia tri cua con tro khi gan gia tri");
     for (int i = 0; i < 5; i++)
     {
         /* code */
        *(pt+i) = i;
-       printf("\ngia tri thu p[%d] = %d ", i, *(pt+i));
+       printf("\np[%d] = %d ", i, *(pt+i));
     }
-
     printf("\n");
 
+    printf("In gia tri cua vung nho");
     for (int j = 0; j <5 ; j++)
     {
         /* code */
         printf("\ngia tri thu p[%d] = %d ", j, *(pt+j));
     }
 
+    printf("\n");
+    printf("\nCap phat them bo nho.\nSo phan tu them vao la N = ");
+    scanf("%d", &N);
+    pt = (int*)realloc(pt, N * sizeof(int));
+    for (int i = 5; i < 5 + N; i++)
+    {
+        /* code */
+       *(pt+i) = i;
+       printf("\np[%d] = %d ", i, *(pt+i));
+    }
+    printf("\nIn gia tri cua vung nho sau khi cap phat them.");
+    for (int i = 0; i <5+N ; i++)
+    {
+        /* code */
+        printf("\ngia tri thu p[%d] = %d ", i, *(pt+i));
+    }
     free(pt);
-    printf("\n");
-
-    /* Sau khi giải phóng bộ nhớ cấp phát cho con trỏ thì giá trị tại nơi con trỏ 
-    trỏ đến là giá trị rác*/
-    for (int j = 0; j <5 ; j++)
-    {
-        /* code */
-        printf("\ngia tri thu p[%d] = %d ", j, *(pt+j));
-    }
-
     return 0;
 }
 
